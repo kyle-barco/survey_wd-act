aa55caef141fb35d98ae343d960c708b8d605f5e
kyle-barco <kylebarco04@gmail.com>
2026-05-02 21:45:45 +0800

    add db

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ require('dotenv').config
 const express = require("express")
 const path = require("node:path")
 
+
+const indexRouter = require('./routes/indexRouter.js')
+
 const app = express()
 app.set("views", path.join(__dirname, "02_views"))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter)
 
 app.listen(3000, (err) => {
   if (err) throw err
