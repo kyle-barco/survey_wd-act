22d7e7d50111a01d8375d9c09c64c7d7ffa0f6e8
kyle-barco <kylebarco04@gmail.com>
2026-05-17 03:49:09 +0800

    edit team routing css and dark mode

--- src/03_controllers/teamController.js
+++ src/03_controllers/teamController.js
@@ -1,9 +1,9 @@
 const teamMembers = [
-    { name: "Kyle Barco", role: "Tech Lead" },
-    { name: "Muzza Lacamento", role: "Front-End Developer" },
-    { name: "Glynicel Vicente", role: "Database Administrator" },
-    { name: "Micah Guda", role: "UI/UX Designer / QA" },
-    { name: "Dave Llaguno", role: "Content & Documentation Specialist" }
+    { name: "Kyle Barco", role: "Backend Developer (Lead)", imgUrl: 'https://avatars.githubusercontent.com/u/101305133?v=4' },
+    { name: "Muzza Lacamento", role: "Frontend Developer", imgUrl: 'https://lh3.googleusercontent.com/a-/ALV-UjXPL6BPe0BElqrUIC_4cRhhzw3L8Rf_V2CjblknpC_QXsEqDCSIiQ=s240-p-k-rw-no' },
+    { name: "Glynicel Vicente", role: "Database Administrator", imgUrl: 'https://lh3.googleusercontent.com/a-/ALV-UjW8c6TFRZYyDfbi8Rvv36TzyDVVGOBqDBd4qsa_LohwfXY4BHw=s240-p-k-rw-no'},
+    { name: "Micah Guda", role: "UI/UX Designer & QA", imgUrl: ''  },
+    { name: "Dave Llaguno", role: "Content & Documentation Specialist", imgUrl: 'resources/img/dave.jpg' }
 ];
 
 exports.showTeam = async (req, res) => {
